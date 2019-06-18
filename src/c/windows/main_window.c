b567e7212d9ed1f08be0483ab7afc8b69248a91b
mattrossman <mrossman@umass.edu>
2019-06-18 11:46:01 -0500

    Separate time layer components

--- src/c/windows/main_window.c
+++ src/c/windows/main_window.c
@@ -1,7 +1,7 @@
 #include "main_window.h"
+#include "../layers/time_layer.h"
 
 static Window *s_main_window;
-static TextLayer *s_time_layer;
 
 static void main_window_load(Window *window) {
     // Get information about the Window
@@ -9,40 +9,12 @@ static void main_window_load(Window *window) {
     GRect bounds = layer_get_bounds(window_layer);
 
     // Create the TextLayer with specific bounds
-    s_time_layer = text_layer_create(
+    time_layer_create(window_layer,
             GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-    // Improve the layout to be more like a watchface
-    text_layer_set_background_color(s_time_layer, GColorClear);
-    text_layer_set_text_color(s_time_layer, GColorBlack);
-    text_layer_set_text(s_time_layer, "00:00");
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-    text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-    // Add it as a child layer to the Window's root layer
-    layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
-    text_layer_destroy(s_time_layer);
-}
-
-static void update_time() {
-    // Get a tm structure
-    time_t temp = time(NULL);
-    struct tm *tick_time = localtime(&temp);
-
-    // Write the current hours and minutes into a buffer
-    static char s_buffer[8];
-    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-            "%H:%M" : "%I:%M", tick_time);
-
-    // Display this time on the TextLayer
-    text_layer_set_text(s_time_layer, s_buffer);
-}
-
-static void minute_handler(struct tm *tick_time, TimeUnits units_changed) {
-    update_time();
+    time_layer_destroy();
 }
 
 /*----------------------------
@@ -59,12 +31,8 @@ void main_window_create() {
         .unload = main_window_unload
     });
 
-    // Register with TickTimerService
-    tick_timer_service_subscribe(MINUTE_UNIT, minute_handler);
-
     // Show the window on the watch with animated=true
     window_stack_push(s_main_window, true);
-    update_time();
 }
 
 void main_window_destroy() {

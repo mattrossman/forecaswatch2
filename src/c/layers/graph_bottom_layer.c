41e7964559aa95080d50b58f91798c8f6964d12b
mattrossman <mrossman@umass.edu>
2019-06-19 18:54:44 -0500

    Add bottom axis line

--- src/c/layers/graph_bottom_layer.c
+++ src/c/layers/graph_bottom_layer.c
@@ -5,22 +5,35 @@ const int s_forecast_start_hour = 6;
 static TextLayer *s_axis_label_layers[6];
 static char s_hour_buf[6][4];
 
+static void graph_bottom_update_proc(Layer *layer, GContext *ctx) {
+    // Weather section outline
+    GRect bounds = layer_get_bounds(layer);
+    graphics_context_set_stroke_color(ctx, GColorWhite);
+    graphics_draw_line(ctx, GPointZero, GPoint(bounds.size.w, 0));
+}
+
 void graph_bottom_layer_create(Layer* parent_layer, GRect frame) {
     s_graph_bottom_layer = layer_create(frame);
     GRect bounds = layer_get_bounds(s_graph_bottom_layer);
-    const int s_entry_width = 20;
+
+    // Set up contents
+    layer_set_update_proc(s_graph_bottom_layer, graph_bottom_update_proc);
+
+    const int s_entry_width = 10;
     for (int i = 0; i < 6; ++i) {
-        TextLayer *text_layer = text_layer_create(GRect(i * s_entry_width, 0, s_entry_width, 20));
+        TextLayer *text_layer = text_layer_create(GRect(i * 2 * s_entry_width, -3, s_entry_width * 2, 20));
         s_axis_label_layers[i] = text_layer;
-        int hour = s_forecast_start_hour + i * 2;
+
+        // Hour labels formatting
         text_layer_set_background_color(text_layer, GColorClear);
         text_layer_set_text_color(text_layer, GColorWhite);
         text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
         text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-        snprintf(s_hour_buf[i], sizeof(s_hour_buf[i]), "%d", hour);
-        APP_LOG(APP_LOG_LEVEL_DEBUG, "(%d) hour: %d", i, hour);
-        APP_LOG(APP_LOG_LEVEL_DEBUG, "(%d) buffer: %s", i, s_hour_buf[i]);
 
+
+        // Write the (int) hour to a character buffer and point the TextLayer at it
+        int hour = s_forecast_start_hour + i * 2;
+        snprintf(s_hour_buf[i], sizeof(s_hour_buf[i]), "%d", hour);
         text_layer_set_text(text_layer, s_hour_buf[i]);
         layer_add_child(s_graph_bottom_layer, text_layer_get_layer(text_layer));
     }

4e92d25c815ad2f632355fe9bfe4ba984302576f
mattrossman <mrossman@umass.edu>
2019-06-27 19:48:05 -0500

    Adjust spacing of forecast lo/hi area

--- src/c/layers/forecast_layer.c
+++ src/c/layers/forecast_layer.c
@@ -30,17 +30,17 @@ void forecast_layer_create(Layer *parent_layer, GRect frame) {
     GRect bounds = layer_get_bounds(s_forecast_layer);
 
     // Temperature HIGH
-    s_hi_layer = text_layer_create(GRect(0, 0, 20, 20));
+    s_hi_layer = text_layer_create(GRect(0, -5, 15, 20));
     text_layer_set_background_color(s_hi_layer, GColorClear);
-    text_layer_set_text_alignment(s_hi_layer, GTextAlignmentCenter);
+    text_layer_set_text_alignment(s_hi_layer, GTextAlignmentRight);
     text_layer_set_text_color(s_hi_layer, GColorWhite);
     text_layer_set_font(s_hi_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     layer_add_child(s_forecast_layer, text_layer_get_layer(s_hi_layer));
 
     // Temperature LOW
-    s_lo_layer = text_layer_create(GRect(0, 20, 20, 20));
+    s_lo_layer = text_layer_create(GRect(0, 25, 15, 20));
     text_layer_set_background_color(s_lo_layer, GColorClear);
-    text_layer_set_text_alignment(s_lo_layer, GTextAlignmentCenter);
+    text_layer_set_text_alignment(s_lo_layer, GTextAlignmentRight);
     text_layer_set_text_color(s_lo_layer, GColorWhite);
     text_layer_set_font(s_lo_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     layer_add_child(s_forecast_layer, text_layer_get_layer(s_lo_layer));
@@ -49,7 +49,7 @@ void forecast_layer_create(Layer *parent_layer, GRect frame) {
     temp_lo_hi_draw();
 
     // Set up contents
-    graph_layer_create(s_forecast_layer, GRect(20, 0, bounds.size.w-20, bounds.size.h));
+    graph_layer_create(s_forecast_layer, GRect(17, 0, bounds.size.w-17, bounds.size.h));
 
     // Add it as a child layer to the Window's root layer
     layer_add_child(parent_layer, s_forecast_layer);

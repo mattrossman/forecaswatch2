9b0886aa2bcf9cd9c822a74431fc6db6be231076
mattrossman <mrossman@umass.edu>
2019-06-29 18:05:55 -0500

    Fill correct day of the month values in calendar

--- src/c/layers/calendar_layer.c
+++ src/c/layers/calendar_layer.c
@@ -1,4 +1,5 @@
 #include "calendar_layer.h"
+#include <time.h>
 
 #define NUM_WEEKS 3
 #define DAYS_PER_WEEK 7
@@ -6,6 +7,15 @@
 
 static Layer *s_calendar_layer;
 static TextLayer *s_calendar_text_layers[NUM_WEEKS * DAYS_PER_WEEK];
+static char calendar_box_buffers[NUM_WEEKS * DAYS_PER_WEEK][4];
+
+static int relative_day_of_month(int days_from_today) {
+    // What is the day of the month relative to today?
+    time_t timestamp = time(NULL);
+    timestamp += days_from_today * SECONDS_PER_DAY;
+    tm *local_time = localtime(&timestamp);
+    return local_time->tm_mday;
+}
 
 void calendar_layer_create(Layer* parent_layer, GRect frame) {
     APP_LOG(APP_LOG_LEVEL_INFO, "Creating calendar layer...");
@@ -16,20 +26,28 @@ void calendar_layer_create(Layer* parent_layer, GRect frame) {
     float box_w = (float) w / DAYS_PER_WEEK;
     float box_h = (float) h / NUM_WEEKS;
 
+    // Calculate which box holds today's date
+    time_t today = time(NULL);
+    struct tm *tm_today = localtime(&today);
+    const int i_today = 7 + tm_today->tm_wday;
+
     for (int i = 0; i < NUM_WEEKS * DAYS_PER_WEEK; ++i) {
         // Place a text box in that space
         TextLayer *s_box_text_layer = text_layer_create(
             GRect((i % DAYS_PER_WEEK) * box_w, (i / DAYS_PER_WEEK) * box_h - FONT_OFFSET,
                   box_w, box_h + FONT_OFFSET));
+        char *buffer = calendar_box_buffers[i];
         text_layer_set_background_color(s_box_text_layer, GColorClear);
         text_layer_set_text_alignment(s_box_text_layer, GTextAlignmentCenter);
         text_layer_set_text_color(s_box_text_layer, GColorWhite);
         text_layer_set_font(s_box_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
-        text_layer_set_text(s_box_text_layer, "16");
+        snprintf(buffer, 4, "%d", relative_day_of_month(i - i_today));  
+        text_layer_set_text(s_box_text_layer, buffer);
         s_calendar_text_layers[i] = s_box_text_layer;
         layer_add_child(s_calendar_layer, text_layer_get_layer(s_box_text_layer));
     }
     layer_add_child(parent_layer, s_calendar_layer);
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "The DOM 0 days from today is: %d", relative_day_of_month(0));
 }
 
 void calendar_layer_refresh() {

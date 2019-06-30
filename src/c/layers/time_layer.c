2c7535e037aad3a68f1a300a1e24dfbb60713eaf
mattrossman <mrossman@umass.edu>
2019-06-30 16:50:11 -0500

    Remove leading zero from time

--- src/c/layers/time_layer.c
+++ src/c/layers/time_layer.c
@@ -24,7 +24,7 @@ void time_layer_refresh() {
     // Write the current hours and minutes into a buffer
     static char s_buffer[8];
     strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-            "%H:%M" : "%I:%M", tick_time);
+            "%k:%M" : "%l:%M", tick_time);
 
     // Display this time on the TextLayer
     text_layer_set_text(s_time_layer, s_buffer);

7001742e052d0216e0d53870538cf4c7e3da58e5
mattrossman <mrossman@umass.edu>
2019-06-30 15:41:29 -0500

    Add battery state handler and draw colored level

--- src/c/layers/battery_layer.c
+++ src/c/layers/battery_layer.c
@@ -1,4 +1,5 @@
 #include "battery_layer.h"
+#include "c/appendix/persist.h"
 
 #define BATTERY_HEIGHT 10
 #define BATTERY_NUB_SIZE 2  // Size of the nub on the right side of the battery
@@ -6,18 +7,27 @@
 
 static Layer *s_battery_layer;
 
+static void battery_state_handler(BatteryChargeState charge) {
+    persist_set_battery_level(charge.charge_percent);
+    battery_layer_refresh();
+}
+
 static void battery_update_proc(Layer *layer, GContext *ctx) {
     GRect bounds = layer_get_bounds(layer);
     int w = bounds.size.w;
     int h = bounds.size.h;
 
+    float battery_w = w * (float) persist_get_battery_level() / 100.0;
+    graphics_context_set_fill_color(ctx, GColorGreen);
+    graphics_fill_rect(ctx, GRect(0, 0, battery_w, BATTERY_HEIGHT), 0, GCornerNone);
     graphics_context_set_stroke_color(ctx, GColorWhite);
-    graphics_draw_rect(ctx, GRect(0,0,w,BATTERY_HEIGHT));
+    graphics_draw_rect(ctx, GRect(0, 0, w, BATTERY_HEIGHT));
 }
 
 void battery_layer_create(Layer* parent_layer, GRect frame) {
     s_battery_layer = layer_create(frame);
     layer_set_update_proc(s_battery_layer, battery_update_proc);
+    battery_state_service_subscribe(battery_state_handler);
     layer_add_child(parent_layer, s_battery_layer);
 }
 
@@ -26,5 +36,6 @@ void battery_layer_refresh() {
 }
 
 void battery_layer_destroy() {
+    battery_state_service_unsubscribe();
     layer_destroy(s_battery_layer);
 }

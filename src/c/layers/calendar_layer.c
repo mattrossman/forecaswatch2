e7f097e2f73f5e14b795abe8dcc9b9cf535f8291
mattrossman <mrossman@umass.edu>
2019-06-30 13:19:57 -0500

    Increase calendar day font size

--- src/c/layers/calendar_layer.c
+++ src/c/layers/calendar_layer.c
@@ -3,7 +3,7 @@
 
 #define NUM_WEEKS 3
 #define DAYS_PER_WEEK 7
-#define FONT_OFFSET 2
+#define FONT_OFFSET 5
 
 static Layer *s_calendar_layer;
 static TextLayer *s_calendar_text_layers[NUM_WEEKS * DAYS_PER_WEEK];
@@ -44,7 +44,7 @@ void calendar_layer_create(Layer* parent_layer, GRect frame) {
         text_layer_set_background_color(s_box_text_layer, GColorClear);
         text_layer_set_text_alignment(s_box_text_layer, GTextAlignmentCenter);
         text_layer_set_text_color(s_box_text_layer, GColorWhite);
-        text_layer_set_font(s_box_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+        text_layer_set_font(s_box_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
         s_calendar_text_layers[i] = s_box_text_layer;
         layer_add_child(s_calendar_layer, text_layer_get_layer(s_box_text_layer));
     }

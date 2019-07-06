033127a4c4fb7f5c7d52f8f81a052d6ea239737a
mattrossman <mrossman@umass.edu>
2019-07-06 15:54:57 -0500

    Convert DarkSkyProvider to ES5

--- src/pkjs/weather.js
+++ src/pkjs/weather.js
@@ -98,39 +98,41 @@ WeatherProvider.prototype.getPayload = function () {
     return payload;
 }
 
-class DarkSkyProvider extends WeatherProvider {
-    constructor(apiKey) {
-        super();
-        this.apiKey = apiKey;
-    }
+var DarkSkyProvider = function (apiKey) {
+    this._super.call(this);
+    this.apiKey = apiKey;
+}
 
-    withDarkSkyResponse(lat, lon, callback) {
-        // callback(darkSkyResponse)
-        var url = 'https://api.darksky.net/forecast/' + this.apiKey + '/' + lat + ',' + lon + '?exclude=minutely,daily,alerts,flags';
-        request(url, 'GET', function (response) {
-            var weatherData = JSON.parse(response);
-            console.log('Found timezone: ' + weatherData.timezone);
-            callback(weatherData);
-        });
-    }
+DarkSkyProvider.prototype = Object.create(WeatherProvider.prototype);
+DarkSkyProvider.prototype.constructor = DarkSkyProvider;
+DarkSkyProvider.prototype._super = WeatherProvider;
 
-    withProviderData(lat, lon, callback) {
-        // callBack expects that this.hasValidData() will be true
-        console.log('This is the overriden implementation of withProviderData')
-        this.tempTrend = [2, 2, 2, 4, 7, 9, 11, 12, 12, 12, 11, 9];
-        this.precipTrend = [2, 2, 2, 4, 7, 9, 11, 12, 12, 12, 11, 9];
-        this.startHour = 6;
-        this.withDarkSkyResponse(lat, lon, (function(darkSkyResponse) {
-            this.tempTrend = darkskyReponse.hourly.data.map(function(entry) {
-                return entry.temperature;
-            })
-            this.precipTrend = darkskyReponse.hourly.data.map(function(entry) {
-                return entry.precipProbability;
-            })
-            this.startHour = new Date(darkskyReponse.hourly.data[0].time * 1000).getHours()
-            callback();
-        }).bind(this));
-    }
+DarkSkyProvider.prototype.withDarkSkyResponse = function (lat, lon, callback) {
+    // callback(darkSkyResponse)
+    var url = 'https://api.darksky.net/forecast/' + this.apiKey + '/' + lat + ',' + lon + '?exclude=minutely,daily,alerts,flags';
+    request(url, 'GET', function (response) {
+        var weatherData = JSON.parse(response);
+        console.log('Found timezone: ' + weatherData.timezone);
+        callback(weatherData);
+    });
+}
+
+DarkSkyProvider.prototype.withProviderData = function (lat, lon, callback) {
+    // callBack expects that this.hasValidData() will be true
+    console.log('This is the overriden implementation of withProviderData')
+    this.tempTrend = [2, 2, 2, 4, 7, 9, 11, 12, 12, 12, 11, 9];
+    this.precipTrend = [2, 2, 2, 4, 7, 9, 11, 12, 12, 12, 11, 9];
+    this.startHour = 6;
+    this.withDarkSkyResponse(lat, lon, (function (darkSkyResponse) {
+        this.tempTrend = darkskyReponse.hourly.data.map(function (entry) {
+            return entry.temperature;
+        })
+        this.precipTrend = darkskyReponse.hourly.data.map(function (entry) {
+            return entry.precipProbability;
+        })
+        this.startHour = new Date(darkskyReponse.hourly.data[0].time * 1000).getHours()
+        callback();
+    }).bind(this));
 }
 
 module.exports.DarkSkyProvider = DarkSkyProvider;

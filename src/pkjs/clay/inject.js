a4ed9fb6d6d7121fef584cc968c28ab900abea8c
mattrossman <mrossman@umass.edu>
2019-07-21 00:47:34 -0500

    Make Dark Sky API key box react to radio selection

--- src/pkjs/clay/inject.js
+++ src/pkjs/clay/inject.js
@@ -7,5 +7,25 @@ module.exports = function (minified) {
         btnUpdateWeather.on('click', function() {
             $('#lastFetchSpan').ht('Now!');
         })
+        var clayDarkSkyApiKey = clayConfig.getItemByMessageKey('darkSkyApiKey');
+        var clayProvider = clayConfig.getItemByMessageKey('provider');
+        var oldProviderValue = clayProvider.get();
+
+        // Configure default provide section layout
+        if (clayProvider.get() !== 'darksky') {
+            clayDarkSkyApiKey.hide()
+        }
+
+        // Configure logic for updating the provider section layout
+        clayProvider.on('change', function() {
+            if (this.get() === 'darksky') {
+                clayDarkSkyApiKey.show();
+            }
+            if (this.get() !== 'darksky' && oldProviderValue === 'darksky') {
+                clayDarkSkyApiKey.hide();
+            }
+            console.log('Provider set to ' + this.get());
+            oldProviderValue = this.get();
+        })
     });
 };

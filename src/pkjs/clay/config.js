3ffe75d6be1e47a781335abcc4b66043416b1293
mattrossman <mrossman@umass.edu>
2019-10-07 17:26:52 -0400

    Reorganize clay components
    Moving the shorter sections up top so they don't get drowned out by the
    big weather section

--- src/pkjs/clay/config.js
+++ src/pkjs/clay/config.js
@@ -13,65 +13,6 @@ module.exports = [
         "items": [
             {
                 "type": "heading",
-                "defaultValue": "Weather"
-            },
-            {
-                "type": "select",
-                "defaultValue": "f",
-                "messageKey": "temperatureUnits",
-                "label": "Temperature Units",
-                "options": [
-                    {
-                        "label": "°F",
-                        "value": "f"
-                    },
-                    {
-                        "label": "°C",
-                        "value": "c"
-                    }
-                ]
-            },
-            {
-                "type": "radiogroup",
-                "label": "Provider",
-                "messageKey": "provider",
-                "defaultValue": "wunderground",
-                "options": [
-                    {
-                        "label": "Weather Underground",
-                        "value": "wunderground"
-                    },
-                    {
-                        "label": "Dark Sky",
-                        "value": "darksky"
-                    }
-                ]
-            },
-            {
-                "type": "input",
-                "label": "Dark Sky API key",
-                "messageKey": "darkSkyApiKey",
-                "description": "<a href='https://darksky.net/dev/register'>Register a Dark Sky developer account</a> and paste your secret key here"
-            },
-            {
-                "type": "toggle",
-                "label": "Force weather fetch",
-                "messageKey": "fetch",
-                "description": "Last successful fetch:<br><span id='lastFetchSpan'>Never :(</span>"
-            },
-            {
-                "type": "input",
-                "label": "Location override",
-                "messageKey": "location",
-                "description": "Leave this blank to use GPS"
-            }
-        ]
-    },
-    {
-        "type": "section",
-        "items": [
-            {
-                "type": "heading",
                 "defaultValue": "Time",
             },
             {
@@ -127,6 +68,65 @@ module.exports = [
                         "value": "mon"
                     }
                 ]
+            }
+        ]
+    },
+    {
+        "type": "section",
+        "items": [
+            {
+                "type": "heading",
+                "defaultValue": "Weather"
+            },
+            {
+                "type": "select",
+                "defaultValue": "f",
+                "messageKey": "temperatureUnits",
+                "label": "Temperature Units",
+                "options": [
+                    {
+                        "label": "°F",
+                        "value": "f"
+                    },
+                    {
+                        "label": "°C",
+                        "value": "c"
+                    }
+                ]
+            },
+            {
+                "type": "radiogroup",
+                "label": "Provider",
+                "messageKey": "provider",
+                "defaultValue": "wunderground",
+                "options": [
+                    {
+                        "label": "Weather Underground",
+                        "value": "wunderground"
+                    },
+                    {
+                        "label": "Dark Sky",
+                        "value": "darksky"
+                    }
+                ]
+            },
+            {
+                "type": "input",
+                "label": "Dark Sky API key",
+                "messageKey": "darkSkyApiKey",
+                "description": "<a href='https://darksky.net/dev/register'>Register a Dark Sky developer account</a> and paste your secret key here"
+            },
+            {
+                "type": "toggle",
+                "label": "Force weather fetch",
+                "messageKey": "fetch",
+                "description": "Last successful fetch:<br><span id='lastFetchSpan'>Never :(</span>"
+            },
+            {
+                "type": "input",
+                "label": "Location override",
+                "messageKey": "location",
+                "description": "Leave this blank to use GPS"
             }
         ]
     },

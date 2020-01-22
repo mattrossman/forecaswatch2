70b69491351a1ac4ed4e086ed53130ac48b14eca
mattrossman <mrossman@umass.edu>
2020-01-22 11:29:17 -0500

    Add bt icon resources, demo connected icon

--- src/c/layers/calendar_status_layer.c
+++ src/c/layers/calendar_status_layer.c
@@ -11,9 +11,11 @@
 static Layer *s_calendar_status_layer;
 static TextLayer *s_calendar_month_layer;
 static TextLayer *s_calendar_month_layer;
-static GBitmap *s_bitmap;
-static BitmapLayer *s_bitmap_layer;
-static GColor *s_mute_palette;
+static GBitmap *s_mute_bitmap;
+static GBitmap *s_bt_bitmap;
+static BitmapLayer *s_mute_bitmap_layer;
+static BitmapLayer *s_bt_bitmap_layer;
+static GColor *s_bt_palette;
 
 void calendar_status_layer_create(Layer* parent_layer, GRect frame) {
     s_calendar_status_layer = layer_create(frame);
@@ -22,19 +24,22 @@ void calendar_status_layer_create(Layer* parent_layer, GRect frame) {
     int h = bounds.size.h;
 
     // Set up icons
-    GBitmap *s_bitmap_temp = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MUTE);
-    s_bitmap = gbitmap_create_palettized_from_1bit(s_bitmap_temp);
-    gbitmap_destroy(s_bitmap_temp);
+    s_mute_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MUTE);
+    s_bt_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_CONNECT);
     
     // Set up color palette
-    s_mute_palette = malloc(2*sizeof(GColor));
-    s_mute_palette[0] = GColorWhite;
-    s_mute_palette[1] = GColorClear;
-    gbitmap_set_palette(s_bitmap, s_mute_palette, false);
+    s_bt_palette = malloc(2*sizeof(GColor));
+    s_bt_palette[0] = GColorPictonBlue;
+    s_bt_palette[1] = GColorClear;
+    gbitmap_set_palette(s_bt_bitmap, s_bt_palette, false);
 
-    s_bitmap_layer = bitmap_layer_create(GRect(PADDING, 0, h, h));
-    bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
-    bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+    s_mute_bitmap_layer = bitmap_layer_create(GRect(PADDING, 0, h, h));
+    bitmap_layer_set_compositing_mode(s_mute_bitmap_layer, GCompOpSet);
+    bitmap_layer_set_bitmap(s_mute_bitmap_layer, s_mute_bitmap);
+
+    s_bt_bitmap_layer = bitmap_layer_create(GRect(PADDING * 2 + 10, 0, h, h));
+    bitmap_layer_set_compositing_mode(s_bt_bitmap_layer, GCompOpSet);
+    bitmap_layer_set_bitmap(s_bt_bitmap_layer, s_bt_bitmap);
 
 
     // Set up month text layer
@@ -45,7 +50,8 @@ void calendar_status_layer_create(Layer* parent_layer, GRect frame) {
     text_layer_set_font(s_calendar_month_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
 
     calendar_status_layer_refresh();
-    layer_add_child(s_calendar_status_layer, bitmap_layer_get_layer(s_bitmap_layer));
+    layer_add_child(s_calendar_status_layer, bitmap_layer_get_layer(s_mute_bitmap_layer));
+    layer_add_child(s_calendar_status_layer, bitmap_layer_get_layer(s_bt_bitmap_layer));
     layer_add_child(s_calendar_status_layer, text_layer_get_layer(s_calendar_month_layer));
     battery_layer_create(s_calendar_status_layer, GRect(w - BATTERY_W - PADDING, 1, BATTERY_W, BATTERY_H));
     layer_add_child(parent_layer, s_calendar_status_layer);
@@ -53,7 +59,8 @@ void calendar_status_layer_create(Layer* parent_layer, GRect frame) {
 
 void status_icons_refresh() {
     bool show = config_show_qt() && quiet_time_is_active();
-    layer_set_hidden(bitmap_layer_get_layer(s_bitmap_layer), !show);
+    show = true;
+    layer_set_hidden(bitmap_layer_get_layer(s_mute_bitmap_layer), !show);
 }
 
 void calendar_status_layer_refresh() {
@@ -67,8 +74,9 @@ void calendar_status_layer_refresh() {
 }
 
 void calendar_status_layer_destroy() {
-    free(s_mute_palette);
-    gbitmap_destroy(s_bitmap);
-    bitmap_layer_destroy(s_bitmap_layer);
+    free(s_bt_palette);
+    gbitmap_destroy(s_mute_bitmap);
+    gbitmap_destroy(s_bt_bitmap);
+    bitmap_layer_destroy(s_mute_bitmap_layer);
     layer_destroy(s_calendar_status_layer);
 }

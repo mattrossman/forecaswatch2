802e2a823b371c82579542265a18d0ea5e14bf4c
mattrossman <mrossman@umass.edu>
2020-02-29 15:22:58 -0500

    Tweak example and provide link to test geocoder

--- src/pkjs/clay/config.js
+++ src/pkjs/clay/config.js
@@ -163,7 +163,10 @@ module.exports = [
                 "type": "input",
                 "label": "Location override",
                 "messageKey": "location",
-                "description": "Leave this blank to use GPS (ensure GPS is enabled if left blank), example: 40.758896,-73.985130"
+                "description": "Example: \"Manhattan\" or \"123 Oak St Plainsville KY\".<br><a href=\"https://locationiq.com/#demo\">Click here</a> to test out your location query.<br>To use GPS, leave this blank and ensure GPS is enabled on your device.",
+                "attributes": {
+                    "placeholder": "Using GPS",
+                }
             }
         ]
     },

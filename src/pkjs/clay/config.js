89049be3b353c68baa8f1875b3eb471f4bf2e331
mattrossman <matthewjonrossman@gmail.com>
2022-07-26 22:54:13 -0400

    Update LocationIQ demo link
    Resolves #58

--- src/pkjs/clay/config.js
+++ src/pkjs/clay/config.js
@@ -212,7 +212,7 @@ module.exports = [
                 "type": "input",
                 "label": "Location override",
                 "messageKey": "location",
-                "description": "Example: \"Manhattan\" or \"123 Oak St Plainsville KY\".<br><a href=\"https://locationiq.com/#demo\">Click here</a> to test out your location query.<br>To use GPS, leave this blank and ensure GPS is enabled on your device.",
+                "description": "Example: \"Manhattan\" or \"123 Oak St Plainsville KY\".<br><a href=\"https://locationiq.com/demo\">Click here</a> to test out your location query.<br>To use GPS, leave this blank and ensure GPS is enabled on your device.",
                 "attributes": {
                     "placeholder": "Using GPS",
                 }

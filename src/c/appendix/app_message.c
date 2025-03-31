dd3b6f639ac41d87511eb860b56ebac19fc4fbff
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2025-03-31 19:59:05 -0400

    Populate "vibe" config value in C message callback
    Resolves #72

--- src/c/appendix/app_message.c
+++ src/c/appendix/app_message.c
@@ -25,6 +25,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     Tuple *clay_prev_week_tuple = dict_find(iterator, MESSAGE_KEY_CLAY_PREV_WEEK);
     Tuple *clay_color_today_tuple = dict_find(iterator, MESSAGE_KEY_CLAY_COLOR_TODAY);
     Tuple *clay_time_font_tuple = dict_find(iterator, MESSAGE_KEY_CLAY_TIME_FONT);
+    Tuple *clay_vibe_tuple = dict_find(iterator, MESSAGE_KEY_CLAY_VIBE);
     Tuple *clay_show_qt_tuple = dict_find(iterator, MESSAGE_KEY_CLAY_SHOW_QT);
     Tuple *clay_show_bt_tuple = dict_find(iterator, MESSAGE_KEY_CLAY_SHOW_BT);
     Tuple *clay_show_bt_disconnect_tuple = dict_find(iterator, MESSAGE_KEY_CLAY_SHOW_BT_DISCONNECT);
@@ -59,7 +60,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
         weather_status_layer_refresh();
     }
     else if (clay_celsius_tuple && clay_axis_12h_tuple && clay_start_mon_tuple && clay_prev_week_tuple && clay_color_today_tuple
-        && clay_show_qt_tuple && clay_show_bt_tuple && clay_show_bt_disconnect_tuple && clay_show_am_pm_tuple
+        && clay_vibe_tuple && clay_show_qt_tuple && clay_show_bt_tuple && clay_show_bt_disconnect_tuple && clay_show_am_pm_tuple
         && clay_color_saturday_tuple && clay_color_sunday_tuple && clay_color_us_federal_tuple && clay_color_time_tuple) {
         // Clay config data received
         bool clay_celsius = (bool) (clay_celsius_tuple->value->int16);
@@ -67,6 +68,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
         bool axis_12h = (bool) (clay_axis_12h_tuple->value->int16);
         bool start_mon = (bool) (clay_start_mon_tuple->value->int16);
         bool prev_week = (bool) (clay_prev_week_tuple->value->int16);
+        bool vibe = (bool) (clay_vibe_tuple->value->int16);
         bool show_qt = (bool) (clay_show_qt_tuple->value->int16);
         bool show_bt = (bool) (clay_show_bt_tuple->value->int16);
         bool show_bt_disconnect = (bool) (clay_show_bt_disconnect_tuple->value->int16);
@@ -85,6 +87,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
             .prev_week = prev_week,
             .time_font = time_font,
             .color_today = color_today,
+            .vibe = vibe,
             .show_qt = show_qt,
             .show_bt = show_bt,
             .show_bt_disconnect = show_bt_disconnect,

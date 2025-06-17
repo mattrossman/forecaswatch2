4cee25f865cda0ac7583cd29ffd319ce94c83187
Alexander Pärsch <alexander.paersch@gmail.com>
2025-06-17 12:04:38 +0200

    Add openholidays setting to index.js

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -80,6 +80,8 @@ function refreshProvider() {
     app.provider.location = app.settings.location === '' ? null : app.settings.location
     app.provider.riduckUser = app.settings.riDuckUsername;
     app.provider.riduckPassword = app.settings.riDuckPassword;
+    app.openHolidaysCountry = app.settings.openHolidaysCountry;
+    app.openHolidaysRegion = app.settings.openHolidaysRegion;
 }
 
 function setProvider(providerId) {

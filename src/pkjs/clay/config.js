5ef91483d2a3dee03b5503d4d05538d19ca73f98
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2026-03-18 00:40:57 -0400

    chore: move telemetry disclosure to subtle footer text

--- src/pkjs/clay/config.js
+++ src/pkjs/clay/config.js
@@ -11,10 +11,6 @@ module.exports = [
         "defaultValue": "Contribute on <a href=\"https://github.com/mattrossman/forecaswatch2\">GitHub!</a>"
     },
     {
-        "type": "text",
-        "defaultValue": "Anonymous telemetry is enabled to improve reliability and understand usage patterns. Learn more in the <a href=\"https://github.com/mattrossman/forecaswatch2#telemetry\">Telemetry section</a>."
-    },
-    {
         "type": "section",
         "items": [
             {
@@ -274,5 +270,9 @@ module.exports = [
     {
         "type": "text",
         "defaultValue": versionLabel
+    },
+    {
+        "type": "text",
+        "defaultValue": "<span style=\"color:#9aa0a6;font-size:0.82em;line-height:1.35;\">Anonymous telemetry is enabled to improve weather reliability and understand usage patterns.<br>Learn more in the <a href=\"https://github.com/mattrossman/forecaswatch2#telemetry\">Telemetry section</a>.</span>"
     }
 ]

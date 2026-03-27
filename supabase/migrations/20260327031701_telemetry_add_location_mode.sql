2d979d35368025e40bd5b9dd7f24ecbd65007bfa
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2026-03-26 23:24:48 -0400

    chore: regenerate telemetry migration diff

--- supabase/migrations/20260327031701_telemetry_add_location_mode.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."telemetry_weather_fetch" add column "location_mode" text;
-
-alter table "public"."telemetry_weather_fetch" add constraint "telemetry_weather_fetch_location_mode_check" CHECK (((location_mode IS NULL) OR (location_mode = ANY (ARRAY['gps'::text, 'manual_coordinates'::text, 'manual_address'::text])))) not valid;
-
-alter table "public"."telemetry_weather_fetch" validate constraint "telemetry_weather_fetch_location_mode_check";
-
-

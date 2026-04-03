7f0577c8d92bf348eb391e882816ad1aef9c4c84
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2026-04-02 23:49:00 -0400

    refactor: centralize and simplify memory probe logging

--- src/c/layers/forecast_layer.c
+++ src/c/layers/forecast_layer.c
@@ -439,26 +439,6 @@ static void draw_night_boundaries_over_precip(GContext *ctx, GRect graph_plot_re
     }
 }
 
-#ifdef FCW2_ENABLE_MEMORY_LOGGING
-typedef struct
-{
-    unsigned long min_free;
-    unsigned long used_at_min;
-} ForecastHeapProbe;
-
-static ForecastHeapProbe forecast_heap_probe_start(int num_entries);
-static void forecast_heap_probe_sample(const char *tag, ForecastHeapProbe *probe);
-static void forecast_heap_probe_log_min(int num_entries, const ForecastHeapProbe *probe);
-
-#define FORECAST_HEAP_SAMPLE(tag, probe_ptr) \
-    forecast_heap_probe_sample(tag, probe_ptr)
-#define FORECAST_HEAP_LOG_MIN(entries, probe_ptr) \
-    forecast_heap_probe_log_min(entries, probe_ptr)
-#else
-#define FORECAST_HEAP_SAMPLE(tag, probe_ptr) do { } while (0)
-#define FORECAST_HEAP_LOG_MIN(entries, probe_ptr) do { } while (0)
-#endif
-
 static void forecast_update_proc(Layer *layer, GContext *ctx)
 {
     MEMORY_LOG_HEAP("forecast_update:enter");
@@ -472,14 +452,11 @@ static void forecast_update_proc(Layer *layer, GContext *ctx)
 
     // Load data from storage
     const int num_entries = persist_get_num_entries();
-#ifdef FCW2_ENABLE_MEMORY_LOGGING
-    ForecastHeapProbe redraw_probe = forecast_heap_probe_start(num_entries);
-#endif
+    MemoryHeapProbe redraw_probe = MEMORY_HEAP_PROBE_START("forecast_update");
     if (num_entries < 2)
     {
         graphics_context_set_fill_color(ctx, GColorBlack);
         graphics_fill_rect(ctx, bounds, 0, GCornerNone);
-        FORECAST_HEAP_LOG_MIN(num_entries, &redraw_probe);
         MEMORY_LOG_HEAP("forecast_update:exit");
         return;
     }
@@ -565,14 +542,14 @@ static void forecast_update_proc(Layer *layer, GContext *ctx)
     GPathInfo path_info_precip = {
         .num_points = num_entries + 2,
         .points = points_precip};
-    FORECAST_HEAP_SAMPLE("forecast_update:before_precip_path_create", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("before_precip_path_create", &redraw_probe);
     GPath *path_precip_area_under = gpath_create(&path_info_precip);
-    FORECAST_HEAP_SAMPLE("forecast_update:after_precip_path_create", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("after_precip_path_create", &redraw_probe);
     graphics_context_set_fill_color(ctx, PRECIP_FILL_COLOR);
     gpath_draw_filled(ctx, path_precip_area_under);
-    FORECAST_HEAP_SAMPLE("forecast_update:before_precip_path_destroy", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("before_precip_path_destroy", &redraw_probe);
     gpath_destroy(path_precip_area_under);
-    FORECAST_HEAP_SAMPLE("forecast_update:after_precip_path_destroy", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("after_precip_path_destroy", &redraw_probe);
 
     if (render_spec.draw_night_overlay)
     {
@@ -584,29 +561,29 @@ static void forecast_update_proc(Layer *layer, GContext *ctx)
 
     // Draw the precipitation line
     path_info_precip.num_points = num_entries;
-    FORECAST_HEAP_SAMPLE("forecast_update:before_precip_top_create", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("before_precip_top_create", &redraw_probe);
     GPath *path_precip_top = gpath_create(&path_info_precip);
-    FORECAST_HEAP_SAMPLE("forecast_update:after_precip_top_create", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("after_precip_top_create", &redraw_probe);
     graphics_context_set_stroke_color(ctx, GColorPictonBlue);
     graphics_context_set_stroke_width(ctx, 1);
     gpath_draw_outline_open(ctx, path_precip_top);
-    FORECAST_HEAP_SAMPLE("forecast_update:before_precip_top_destroy", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("before_precip_top_destroy", &redraw_probe);
     gpath_destroy(path_precip_top);
-    FORECAST_HEAP_SAMPLE("forecast_update:after_precip_top_destroy", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("after_precip_top_destroy", &redraw_probe);
 
     // Draw the temperature line
     GPathInfo path_info_temp = {
         .num_points = num_entries,
         .points = points_temp};
-    FORECAST_HEAP_SAMPLE("forecast_update:before_temp_path_create", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("before_temp_path_create", &redraw_probe);
     GPath *path_temp = gpath_create(&path_info_temp);
-    FORECAST_HEAP_SAMPLE("forecast_update:after_temp_path_create", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("after_temp_path_create", &redraw_probe);
     graphics_context_set_stroke_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorWhite));
     graphics_context_set_stroke_width(ctx, 3); // Only odd stroke width values supported
     gpath_draw_outline_open(ctx, path_temp);
-    FORECAST_HEAP_SAMPLE("forecast_update:before_temp_path_destroy", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("before_temp_path_destroy", &redraw_probe);
     gpath_destroy(path_temp);
-    FORECAST_HEAP_SAMPLE("forecast_update:after_temp_path_destroy", &redraw_probe);
+    MEMORY_HEAP_PROBE_SAMPLE("after_temp_path_destroy", &redraw_probe);
 
     // Draw a line for the bottom axis
     graphics_context_set_stroke_color(ctx, render_spec.axis_color);
@@ -617,7 +594,7 @@ static void forecast_update_proc(Layer *layer, GContext *ctx)
     graphics_context_set_fill_color(ctx, GColorBlack);
     graphics_fill_rect(ctx, GRect(0, 0, s_axis_left_w, h - BOTTOM_AXIS_H), 0, GCornerNone); // Paint over plot bleeding
     graphics_draw_line(ctx, GPoint(graph_bounds.origin.x, 0), GPoint(graph_bounds.origin.x, axis_y));
-    FORECAST_HEAP_LOG_MIN(num_entries, &redraw_probe);
+    MEMORY_HEAP_PROBE_LOG_MIN(&redraw_probe);
     MEMORY_LOG_HEAP("forecast_update:exit");
 }
 
@@ -663,46 +640,6 @@ static void text_layers_refresh()
     text_layer_set_text(s_hi_layer, s_buffer_hi);
     text_layer_set_text(s_lo_layer, s_buffer_lo);
 }
-
-#ifdef FCW2_ENABLE_MEMORY_LOGGING
-static ForecastHeapProbe forecast_heap_probe_start(const int num_entries)
-{
-    ForecastHeapProbe probe = {
-        .min_free = (unsigned long)heap_bytes_free(),
-        .used_at_min = (unsigned long)heap_bytes_used()};
-
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "MEM|forecast_update:start|entries=%d|free=%lu|used=%lu",
-            num_entries,
-            probe.min_free,
-            probe.used_at_min);
-
-    return probe;
-}
-
-static void forecast_heap_probe_sample(const char *tag, ForecastHeapProbe *probe)
-{
-    const unsigned long free_now = (unsigned long)heap_bytes_free();
-    const unsigned long used_now = (unsigned long)heap_bytes_used();
-    if (free_now < probe->min_free)
-    {
-        probe->min_free = free_now;
-        probe->used_at_min = used_now;
-    }
-
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "MEM|%s|free=%lu|used=%lu",
-            tag,
-            free_now,
-            used_now);
-}
-
-static void forecast_heap_probe_log_min(const int num_entries, const ForecastHeapProbe *probe)
-{
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "MEM|forecast_update:min_free|entries=%d|free=%lu|used=%lu",
-            num_entries,
-            probe->min_free,
-            probe->used_at_min);
-}
-#endif
 
 void forecast_layer_create(Layer *parent_layer, GRect frame)
 {

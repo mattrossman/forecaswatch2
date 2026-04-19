e52073c5614ed69b1760b82be0c592f8352ac031
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2026-04-19 17:31:20 +0200

    fix

--- src/c/appendix/chime.c
+++ src/c/appendix/chime.c
@@ -4,7 +4,7 @@
 void chime_handle(struct tm *tick_time) {
   if (g_config->chime == 30 && tick_time->tm_min == 30) {
     vibes_short_pulse();
-  } else if (g_config->chime == 60 && tick_time->tm_min == 0) {
+  } else if (g_config->chime >= 30 && tick_time->tm_min == 0) {
     vibes_double_pulse();
   }
 }
